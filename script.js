bd02da1b489aaab2bb7542fb624ecdf8dd04ba40
Valeriu234 <cazacuvaleriu2@gmail.com>
2022-07-15 21:13:18 +0100

    Am creat variabile pentru fiecare imagine ingredient

---
+++ script.js
@@ -0,0 +1,8 @@
+// Am creat variabilele pentru ingredientele de sus
+const buns = document.querySelector('.bread');
+const meats = document.querySelector('.meat');
+const cheeses = document.querySelector('.cheese');
+const salad = document.querySelector('.salad');
+const vegetable = document.querySelector('.tomato');
+const sauce = document.querySelector('.sauce');
+
