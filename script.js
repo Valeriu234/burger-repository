3136be84b4ee5812424c52c8b0ba57f718116ccb
Valeriu234 <cazacuvaleriu2@gmail.com>
2022-07-16 20:09:05 +0100

    Nimic

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 
 
-//am create functie care sterge lista  ca sa stearga lista trebuie de adaugat la obiectele comandate de acaugat in tag li clasa order ListRemove
-const deleteLi= document.querySelectorAll('.btn-remove')
-const listaOrdere = document.querySelector('.orderListRemove')
-deleteLi.forEach((e)=>{
-    e.addEventListener('click',()=>{
-        console.log(listaOrdere);
-        listaOrdere.remove()
-    })
-})
